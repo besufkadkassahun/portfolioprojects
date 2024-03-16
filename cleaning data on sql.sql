a7f584f8c8abb2147c67438d0f23f216ce4005ca
besufkadkassahun <163596038+besufkadkassahun@users.noreply.github.com>
2024-03-16 16:50:23 -0600

    Add files via upload
    Data cleaning in SQL involves removing duplicates, handling missing values, correcting data type, standardizing values, formatting data and inconsistent data

--- cleaning data on sql.sql
+++ cleaning data on sql.sql
@@ -1,62 +1,121 @@
--- Data cleaning project
+/*
+  Project - Data Cleaning 
+  Created by - Besufkad Kassahun
+*/
+ Use [portfolio project]
 
-select *
-from [dbo].[dbo.nashvillehousing]
+ -- Data cleaning project
 
---standardize data format
+    Select      *
+    From        [dbo].[dbo.nashvillehousing]
 
-select saledateconverted, convert(date,saledate )
-from [dbo].[dbo.nashvillehousing]
+ --standardize data format
 
-alter table [dbo].[dbo.nashvillehousing]
-add saledateconverted date;
+    Select      saledateconverted, convert(date,saledate )
+    From        [dbo].[dbo.nashvillehousing]
 
-update [dbo].[dbo.nashvillehousing]
-set saledateconverted = convert(date,saledate )
+    Alter table [dbo].[dbo.nashvillehousing]
+          add   saledateconverted date;
 
---populate property address data
+    Update      [dbo].[dbo.nashvillehousing]
+       Set      saledateconverted = convert(date,saledate )
 
-select *
-from [dbo].[dbo.nashvillehousing]
-where [PropertyAddress] is null
+ --populate property address data
 
-   --self join 
-select a.[ParcelID],a.[PropertyAddress],b.[ParcelID],b.[PropertyAddress],
-       isnull(a.[PropertyAddress], b.[PropertyAddress])
-from [dbo].[dbo.nashvillehousing] a
-join [dbo].[dbo.nashvillehousing] b
-   on a.[ParcelID] = b.[ParcelID]
-   and a.[UniqueID ] <> b.[UniqueID ]
-   where a.[PropertyAddress] is null
+    Select      *
+    From        [dbo].[dbo.nashvillehousing]
+    Where       [PropertyAddress] is null
 
-update a
-set [PropertyAddress] = isnull(a.[PropertyAddress], b.[PropertyAddress])
-    from [dbo].[dbo.nashvillehousing] a
-join [dbo].[dbo.nashvillehousing] b
-   on a.[ParcelID] = b.[ParcelID]
-   and a.[UniqueID ] <> b.[UniqueID ]
-   where a.[PropertyAddress] is null
+ --self join 
+  
+    Select      a.[ParcelID],a.[PropertyAddress],b.[ParcelID],b.[PropertyAddress],
+                isnull(a.[PropertyAddress], b.[PropertyAddress])
+    From        [dbo].[dbo.nashvillehousing] a
+    Join        [dbo].[dbo.nashvillehousing] b
+      On        a.[ParcelID] = b.[ParcelID]
+     And        a.[UniqueID ] <> b.[UniqueID ]
+   Where        a.[PropertyAddress] is null
 
---breaking out address into individual columns (adress,city,state)
+  Update        a
+     Set        [PropertyAddress] = isnull(a.[PropertyAddress], b.[PropertyAddress])
+    From        [dbo].[dbo.nashvillehousing] a
+    Join        [dbo].[dbo.nashvillehousing] b
+      on        a.[ParcelID] = b.[ParcelID]
+     And        a.[UniqueID ] <> b.[UniqueID ]
+   Where        a.[PropertyAddress] is null
 
-select [PropertyAddress]
-from [dbo].[dbo.nashvillehousing]
+ --breaking out address into individual columns (adress,city,state)
 
-    --we use sub string and charindex
-select   
-   substring([PropertyAddress], 1,CHARINDEX(',',[PropertyAddress])-1) as address,
-   substring([PropertyAddress],CHARINDEX(',',[PropertyAddress])+1, len([PropertyAddress])) as address
-from [dbo].[dbo.nashvillehousing]
+  Select        [PropertyAddress]
+    From        [dbo].[dbo.nashvillehousing]
 
-alter table [dbo].[dbo.nashvillehousing]
-add propertysplitaddress nvarchar(255);
+    --we use sub string and charindex for the [PropertyAddress]
 
-update [dbo].[dbo.nashvillehousing]
-set propertysplitaddress = substring([PropertyAddress], 1,CHARINDEX(',',[PropertyAddress])-1)
+  Select   
+                substring([PropertyAddress], 1,CHARINDEX(',',[PropertyAddress])-1) as address,
+                substring([PropertyAddress],CHARINDEX(',',[PropertyAddress])+1, len([PropertyAddress])) as address
+    From        [dbo].[dbo.nashvillehousing]
 
-alter table [dbo].[dbo.nashvillehousing]
-add propertysplitcity nvarchar(255);
+    Alter table [dbo].[dbo.nashvillehousing]
+      Add       propertysplitaddress nvarchar(255);
+
+  Update        [dbo].[dbo.nashvillehousing]
+     Set        propertysplitaddress = substring([PropertyAddress], 1,CHARINDEX(',',[PropertyAddress])-1)
+
+   Alter table  [dbo].[dbo.nashvillehousing]
+     Add        propertysplitcity nvarchar(255);
+
+	 --we use sub parsename for the [OwnerAddress]
+
+   Select 
+                parsename(replace([OwnerAddress],',','.'),3),
+	            parsename(replace([OwnerAddress],',','.'),2),
+	            parsename(replace([OwnerAddress],',','.'),1)
+     From       [dbo].[dbo.nashvillehousing]
+
+    Alter table [dbo].[dbo.nashvillehousing]
+      Add       ownersplitaddress nvarchar(255);
+
+    Update      [dbo].[dbo.nashvillehousing]
+       Set      ownersplitaddress = parsename(replace([OwnerAddress],',','.'),3)
+
+     Alter table[dbo].[dbo.nashvillehousing]
+      Add       ownersplitcity nvarchar(255);
+
+    Update      [dbo].[dbo.nashvillehousing]
+       Set      ownersplitcity = parsename(replace([OwnerAddress],',','.'),2)
+
+     Alter table[dbo].[dbo.nashvillehousing]
+       Add      ownersplitstate nvarchar(255);
+
+    Update      [dbo].[dbo.nashvillehousing]
+       Set      ownersplitstate = parsename(replace([OwnerAddress],',','.'),1)
 
 
-select *
-from [dbo].[dbo.nashvillehousing] 
+
+ -- change Y and N to Yes and No in "solid as vacant" field
+
+    Select         [SoldAsVacant],
+      Case when    [SoldAsVacant]= 'y'then 'yes'
+           when    [SoldAsVacant]= 'n' then 'no'
+		   else    [SoldAsVacant]
+	  End
+      From         [dbo].[dbo.nashvillehousing]
+
+    Update         [dbo].[dbo.nashvillehousing]
+       set         [SoldAsVacant] = case when [SoldAsVacant]= 'y'then 'yes'
+      when         [SoldAsVacant] = 'n' then 'no'
+      else         [SoldAsVacant]
+	end
+
+-- Delete unused columns  
+
+     Alter table   [dbo].[dbo.nashvillehousing]
+     Drop column   [OwnerAddress],[TaxDistrict],[PropertyAddress],[SaleDate]
+
+
+
+     Alter table   [dbo].[dbo.nashvillehousing]
+     Drop column   [SaleDate]
+
+ -- End 
